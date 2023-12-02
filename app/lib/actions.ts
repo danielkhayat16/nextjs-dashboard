b3503ec5137d14a19aeb4b302a282f8f6a8a6160
Daniel Khayat <daniel.khayat8@gmail.com>
2023-12-02 19:46:46 +0200

    typo error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,21 +12,21 @@ const FormSchema = z.object({
     date: z.string(),
 })
 
-const CreateInvoice = FormSchema.omit({id: true, date: true});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      });
-      const amountInCents = amount * 100;
-      const date = new Date().toISOString().split('T')[0];
-      await sql`
-        INSERT INTO invoices (customer_id, amountm status, date)
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
 
-        revalidatePath('/dashboard/invoices')
-        redirect('/dashboard/invoices');
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices');
 }
